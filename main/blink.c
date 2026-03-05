e940b0d5b16a064262fd762991db53b11a6de06e
gajera miraj <gajera@miraj.gajera>
2026-03-05 10:12:44 +0530

    Initial ESP-IDF project commit

---
+++ main/blink.c
@@ -0,0 +1,20 @@
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LED_GPIO GPIO_NUM_2
+
+void app_main(void)
+{
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+
+    while (1)
+    {
+        gpio_set_level(LED_GPIO, 1);
+        vTaskDelay(pdMS_TO_TICKS(500));
+
+        gpio_set_level(LED_GPIO, 0);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
